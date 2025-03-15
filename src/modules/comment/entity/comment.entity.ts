9d5ad2d2bf75edadb37e35f45dc8259d09a500fa
Aashutosh Parajuli <aashutosh282@gmail.com>
2025-03-15 08:46:49 +0545

    feat: add comment module with service and entity definitions

---
+++ src/modules/comment/entity/comment.entity.ts
@@ -0,0 +1,25 @@
+import Base from 'src/helpers/base.entity';
+import { Auth } from 'src/modules/auth/entity/auth.entity';
+import { Post } from 'src/modules/posts/entity/posts.entity';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+
+
+@Entity('Comment')
+export class Comment extends Base {
+    @Column()
+    comment: string;
+
+    @ManyToOne(() => Auth, (auth) => auth.posts, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'auth_id' })
+    auth: Auth;
+
+    @ManyToOne(() => Post, (post) => post.comments, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'post_id' })
+    post: Post;
+
+    @ManyToOne(() => Comment, (comment) => comment.children, { nullable: true })
+    parent: Comment;
+
+    @OneToMany(() => Comment, (comment) => comment.parent, { nullable: true })
+    children: Comment[];
+}
